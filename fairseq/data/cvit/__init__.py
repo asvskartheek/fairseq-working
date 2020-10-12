0257580df5f2bff222f4e0edbf6f507281e99bd4
Kartheek Akella <sukruthkartheek@gmail.com>
2020-10-12 09:56:34 +0530

    Update __init__.py
    remove hard-coding of ILMULTI_CORPUS_ROOT

--- fairseq/data/cvit/__init__.py
+++ fairseq/data/cvit/__init__.py
@@ -3,7 +3,7 @@ import warnings
 from collections import namedtuple
 
 ENV_VAR='ILMULTI_CORPUS_ROOT'
-DATA_ROOT = "/content/drive/My Drive/IIIT-H RA/ICON/data"
+DATA_ROOT = os.getenv(ENV_VAR, None)
 if DATA_ROOT is None:
     warnings.warn(
         "Please define {} in environment variable" .format(ENV_VAR)
